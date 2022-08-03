fedf4d57a63ee2b5d16ad32b877415ba732db741
jaeheon1 <jungjaehen@naver.com>
2022-08-03 19:43:58 +0900

    light
    direct spot point

---
+++ unitystart/Assets/Light/Script/Patrol.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    int target = 3;
+   
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float degree;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+            new Vector3(target,0)
+            ,speed*Time.deltaTime);
+
+        if(transform.position.x>=3)
+        {
+            target = -3;
+
+        }
+        else if (transform.position.x<=-3)
+        {
+            target = 3;
+        }
+
+        degree += Time.deltaTime;
+        if(degree>=360)
+        {
+            degree = 0;
+        }
+        RenderSettings.skybox.SetFloat("_Rotation", degree);
+    }
+}
