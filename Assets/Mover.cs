a4df78d21bc6dba8a06ead16a36f07ae14b521ee
jae heon <101865597+jaeheon1@users.noreply.github.com>
2023-04-06 11:09:59 +0900

    1

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,7 +6,7 @@ public class Mover : MonoBehaviour
 {
     [SerializeField]  float yValue = 0.01f;
     [SerializeField] float zValue = 0f;
-   
+    [SerializeField] float speed = 10f;
     void Start()
     {
        
@@ -15,8 +15,8 @@ public class Mover : MonoBehaviour
    
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float yValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        float yValue = Input.GetAxis("Vertical") * Time.deltaTime*speed;
         transform.Translate(xValue, 0, yValue);
     }
 }
